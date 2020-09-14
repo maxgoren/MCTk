4907095dd9f6d05b1758cddfe75e7be5992622a4
Max Goren <65133477+maxgoren@users.noreply.github.com>
2020-09-13 22:55:47 -0400

    Add files via upload

--- src/drunk.cpp
+++ src/drunk.cpp
@@ -1,121 +1,161 @@
-/****************************************************************************
- * MIT License
+#include "landscape.h"
 
-Copyright (c) 2020 Max Goren - maxgoren@icloud.com, https://www.maxcodes.info
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-******************************************************************************/
-
-namespace mctk {
-
-drunk_walk::drunk_walk(zone outline)
+landscape::landscape()
 {
-     this->mapW = outline.mapW;
-     this->mapH = outline.mapH;
-     this->map = outline.layout;
-     this->hasmaze = outline.ismaze;
-     cd[0] = {0, -1}; cd[3] = {0, 1};
-     cd[1] = {-1, 0}; cd[2] = {1, 0};
-     
+
 }
 
-inline bool drunk_walk::onmap(Point curr)
+field landscape::make_it(field layout, int mapW, int mapH)
+{
+    this->mapW = mapW;
+    this->mapH = mapH;
+    int x, y;
+    for (x = 0; x < mapW; x++)
+    {
+        for (y = 0; y < mapH; y++)
+        {
+            layout[x][y] = {x,y,'#', 0, 1};
+            layout[x][y].blocks = true;
+            layout[x][y].populated = false;
+            layout[x][y].color = 0xFFFF0000;
+        }
+    }
+    std::cout<<"calling walk.\n";
+    layout = biased_walk(layout, .53);
+    return layout;
+}
+
+
+
+inline bool landscape::onmap(Point curr)
 {
  return curr.x > 1 && curr.x < mapW && curr.y > 1 && curr.y < mapH; 
 }
 
-void drunk_walk::walk(double toclear)
+field landscape::biased_walk(field layout, float percent_clear)
 {
-  std::stack<Point> backtrack;
-  int stepped = 0;
-  int i;
-  double area = (mapW-2)*(mapH-2);
-  double goal = area * toclear;
-  Point current = {getrand(2,mapW-2), getrand(2,mapH-2)};
-  map[current.x][current.y].blocks = false;
-  stepped++;
-  std::cout<<"First: "<<current.x<<"/"<<current.y<<"\n";
+  std::cout<<"Waling\n";
+  std::array<Point, 4> cd;
+  Point dxdy;
+  Point current;
   Point next;
-  while (stepped < goal)
+  int x;
+  int y;
+  int selector;
+  int stepped = 0;
+  double area = ((double)mapW * (double)mapH) / 4;
+  double to_clear = area * percent_clear;
+  cd[0] = {0, -1}; cd[3] = {0, 1};
+  cd[1] = {-1, 0}; cd[2] = {1, 0};
+  x = aux.getrand(20, mapW-20);
+  y = aux.getrand(10, mapH-10);
+  current = {x,y};
+  layout[x][y].blocks = false;
+  stepped++;
+  while (stepped < to_clear)
   {
-    i = getrand(0,3);
-    next = {current.x + cd[i].x, current.y+cd[i].y};
-    if (onmap(next))
+    selector = aux.getrand(0,3);
+    if (current.x > (mapW-mapW/6))
     {
-           map[next.x][next.y].blocks = false;
-           backtrack.push(next);
-           stepped++;
-           current = next;      
+      if (aux.getrand(1,100) < 10)
+        dxdy = {-1,0};
+      else
+        dxdy = cd[selector];
+    } else if (current.x < mapW/6) {
+      if (aux.getrand(1,100) < 10)
+       dxdy = {1,0};
+      else
+       dxdy = cd[selector];
+    } else if (current.y < 10) {
+      if (aux.getrand(1,100)< 20)
+       dxdy = {0,1};
+      else
+       dxdy = cd[selector];
+    } else if (current.y > mapH-10) {
+      if (aux.getrand(1,100) < 20)
+       dxdy = {0,-1};
+      else
+       dxdy = cd[selector];
     } else {
-      i = getrand(1,5);
-      switch(i)
-      {
-        case 1: 
-                current.x = mapW/2;
-                current.y = mapH/2;
-                break;
-        case 2:
-                current.x = mapW/4;
-                current.x = mapH/4;
-                break;
-        case 3: 
-                current.x = mapW/4;
-                current.y = mapH - mapH/4;
-                break;
-        case 4:
-                current.x = mapW - mapW/4;
-                current.y = mapH/4;
-                break;
-        case 5:
-                current.x = mapW - mapW/4;
-                current.y = mapH - mapH/4;
-      }
+       dxdy = cd[selector];
+    }
+    std::cout<<"Current: "<<current.x<<"/"<<current.y<<" dxdy: "<<dxdy.x<<"/"<<dxdy.y<<"\n";
+    next = {current.x + dxdy.x, current.y + dxdy.y};
+    if (onmap(next)) 
+    { 
+      std::cout<<"Trying: "<<next.x<<"/"<<next.y<<"...";
+     if (layout[next.x][next.y].blocks == true)
+     {
+       layout[next.x][next.y].blocks = false;
+       layout[next.x][next.y].color = 0xFFFF4400;
+       layout[next.x][next.y].s = '-';
+       stepped++;
+       std::cout<<"accepted\n";
+     }
+    }
+    std::cout<<"rejected\n";
+    current = next;
+  }
+ return layout;
+}
+
+
+field landscape::play(field layout)
+{
+  int x, y;
+  for (x = 0; x < 160; x++)
+  {
+    for (y = 0; y < 40; y++)
+    {
+       layout[x][y]={x,y,'#',0,1};
+       layout[x][y].blocks = true;
+       layout[x][y].color = 0xFFCECECE;
     }
   }
-  while (!backtrack.empty())
+  for (x = 2; x < 15; x++)
   {
-      //hit me baby one more time..
-      next = backtrack.top();
-      backtrack.pop();
-      i = getrand(0,3);
-      next = {current.x + cd[i].x, current.y+cd[i].y};
-      if (onmap(next))
-      {
-           map[next.x][next.y].blocks = false;
-           current = next;      
-      }
+    for (y = 3; y < 15; y++)
+    {
+       layout[x][y].s = '.';
+       layout[x][y].blocks = false;
+       layout[x][y].color = 0xFFCEFFCE;
+    }
   }
-}
-
-zone drunk_walk::getZone()
-{
-  zone retzone;
-  retzone.mapW = mapW;
-  retzone.mapH = mapH;
-  retzone.layout = map;
-  retzone.ismaze = hasmaze;
-  int x, y;
-  for (x = 0; x < mapW; x++)
-    for (y = 0; y < mapH; y++)
-      if (!map[x][y].blocks)
-        retzone.openSpace.push_back(map[x][y]);
-  return retzone;
-}
-
+  for (x = 14; x < 55; x++)
+  {
+    for (y = 7; y < 15; y++)
+    {
+       layout[x][y].s = '.';
+       layout[x][y].blocks = false;
+       layout[x][y].color = 0xFFCEFFCE;
+    }
+  }
+  for (x = 20; x < 70; x++)
+  {
+    for (y = 2; y < 6; y++)
+    {
+      layout[x][y].s = '.';
+       layout[x][y].blocks = false;
+       layout[x][y].color = 0xFFCEFFCE;
+    }
+  }
+  for (x = 50; x < 80; x++)
+  {
+    for (y = 10; y < 15; y++)
+    {
+      layout[x][y].s = '.';
+       layout[x][y].blocks = false;
+       layout[x][y].color = 0xFFCEFFCE;
+    }
+  }
+  for (x = 56; x < 64; x++)
+  {
+    for (y = 3; y < 15; y++)
+    {
+      layout[x][y].s = '.';
+       layout[x][y].blocks = false;
+       layout[x][y].color = 0xFFCEFFCE;
+    }
+  }
+  return layout;
 }
